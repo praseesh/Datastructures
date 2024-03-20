bbd07635b629ccd6f26e0982ee635f9d54bf26da
praseesh <praseeshprasee@gmail.com>
2024-03-20 16:48:14 +0530

    duplicates

--- Array/Duplicates.c
+++ Array/Duplicates.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 int main(){
     
-
+    int i,j,list[10],duplicates[10], limit, count=0;
+    printf("Enter limit: ");
+    scanf("%d", &limit);
+    printf("Enter numbers: \n");
+    for (i=0; i<limit; i++){
+        scanf("%d", &list[i]);
+    }
+    
+    for (i=0; i<limit-1; i++){
+        for (j=i+1; j<limit; j++){
+            if (list[i] == list[j]){
+                printf("Duplicate: %d, ",list[j]);
+            }
+        }
+    }
 return 0;
 }

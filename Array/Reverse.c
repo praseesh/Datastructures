b8c33ebb70bed1d7d01497c405c558e9dd95569a
Praseesh <praseeshprasee@gmail.com>
2024-03-26 10:29:47 +0530

    reverse

--- Array/Reverse.c
+++ Array/Reverse.c
@@ -3,7 +3,7 @@ int main(){
     
 
 int i,rev_array;
-int values[] = {3,5,6,65,8,22,52,53,63,65,23,66,66,24,};
+int values[] = {3,5,6,65,8,22,52,53,63,65,23,66,66,24};
 int n = sizeof(values) / sizeof(values[0]);
 for (i=0; i<n; i++){
    printf("%d,",values[i]);
